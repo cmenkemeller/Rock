c92d136e58621d0080915923235dbcaa854152a4
Kwame_Agyei <kwame.ajyei@techvision.global>
2024-09-24 17:13:45 +0000

    - Update Google Maps Lavashortcode to use AdvanceMarkerElement.

---
+++ Rock.Migrations/Migrations/202409241655147_UpdateGoogleMapsLavashortcode.cs
@@ -0,0 +1,450 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+namespace Rock.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    /// <summary>
+    /// Migration will be added as a rollup.
+    /// </summary>
+    public partial class UpdateGoogleMapsLavashortcode : Rock.Migrations.RockMigration
+    {
+        /// <summary>
+        /// Operations to be performed during the upgrade process.
+        /// </summary>
+        public override void Up()
+        {
+            var markup = @"
+{% capture singleQuote %}'{% endcapture %}
+{% capture escapedQuote %}\'{% endcapture %}
+{% assign apiKey = 'Global' | Attribute:'GoogleApiKey' %}
+{% assign url = 'key=' | Append:apiKey %}
+{% assign id = uniqueid | Replace:'-','' %}
+
+{% if apiKey == """" %}
+    <div class=""alert alert-warning"">
+        There is no Google API key defined. Please add your key under: 'Admin Tools > General Settings > Global Attributes > Google API Key'.
+    </div>
+{% endif %}
+
+{% assign markerCount = markers | Size -%}
+
+{% assign mapCenter = center | Trim %}
+{% if mapCenter == """" and markerCount > 0 -%}
+    {% assign centerPoint = markers | First %}
+    {% assign mapCenter = centerPoint.location %}
+{% endif %}
+
+{% assign mapZoom = zoom | Trim %}
+{% if mapZoom == """" %}
+    {% if markerCount == 1 -%}
+        {% assign mapZoom = '11' %}
+    {% else %}
+        {% assign mapZoom = '10' %}
+    {% endif %}
+{% endif %}
+
+{% javascript id:'googlemapsapi' url:'{{ ""https://maps.googleapis.com/maps/api/js?libraries=marker&key="" | Append:apiKey }}' %}{% endjavascript %}
+
+{% case markeranimation %}
+{% when 'drop' %}
+    {% assign markeranimation = 'drop' %}
+{% when 'bounce' %}
+    {% assign markeranimation = 'bounce' %}
+{% else %}
+    {% assign markeranimation = null %}
+{% endcase %}
+
+{% stylesheet %}
+
+.{{ id }} {
+    width: {{ width }};
+}
+
+#map-container-{{ id }} {
+    position: relative;
+}
+
+#{{ id }} {
+    height: {{ height }};
+    overflow: hidden;
+    padding-bottom: 22.25%;
+    padding-top: 30px;
+    position: relative;
+}
+
+@keyframes drop {
+  0% {
+    transform: translateY(-200px) scaleY(0.9);
+    opacity: 0;
+  }
+  5% {
+    opacity: 0.7;
+  }
+  50% {
+    transform: translateY(0px) scaleY(1);
+    opacity: 1;
+  }
+  65% {
+    transform: translateY(-17px) scaleY(0.9);
+    opacity: 1;
+  }
+  75% {
+    transform: translateY(-22px) scaleY(0.9);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(0px) scaleY(1);
+    opacity: 1;
+  }
+}
+
+.drop {
+  animation: drop 0.3s linear forwards .5s;
+}
+
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-30px);
+  }
+  60% {
+    transform: translateY(-15px);
+  }
+}
+
+.bounce {
+  animation: bounce 2s infinite;
+}
+
+{% endstylesheet %}
+
+<div class=""map-container {{ id }}"">
+    <div id=""map-container-{{ id }}""></div>
+    <div id=""{{ id }}""></div>
+</div>	
+
+<script>
+    // create javascript array of marker info
+    var markers{{ id }} = [
+        {% for marker in markers -%}
+            {% assign title = '' -%}
+            {% assign content = '' -%}
+            {% assign icon = '' -%}
+            {% assign location = marker.location | Split:',' -%}
+            {% if marker.title and marker.title != '' -%}
+                {% assign title = marker.title | HtmlDecode | Replace:singleQuote,escapedQuote -%}
+            {% endif -%}
+            {% if marker.content != '' -%}
+                {% assign content = marker.content | StripNewlines | HtmlDecode | Replace:singleQuote,escapedQuote -%}
+            {% endif -%}
+            {% if marker.icon and marker.icon != '' -%}
+                {% assign icon = marker.icon -%}
+            {% endif -%}
+            [{{ location[0] }}, {{ location[1] }},'{{ title }}','{{ content }}','{{ icon }}'],
+        {% endfor -%}
+    ];
+
+    //Set Map
+    function initialize{{ id }}() {
+        var bounds = new google.maps.LatLngBounds();
+        var centerLatLng = new google.maps.LatLng( {{ mapCenter }} );
+        if ( isNaN( centerLatLng.lat() ) || isNaN( centerLatLng.lng() ) ) {
+            centerLatLng = null;
+        };
+
+        var mapOptions = {
+            zoom: {{ mapZoom }},
+            scrollwheel: {{ scrollwheel }},
+            draggable: {{ draggable }},
+            center: centerLatLng,
+            mapTypeId: '{{ maptype }}',
+            zoomControl: {{ showzoom }},
+            mapTypeControl: {{ showmaptype }},
+            gestureHandling: '{{ gesturehandling }}',
+            streetViewControl: {{ showstreetview }},
+            fullscreenControl: {{ showfullscreen }}
+            {% if style and style.content != """" %}
+                ,styles: {{ style.content | StripNewlines | Trim }}
+            {% endif %},
+	    mapId: 'lava_google_map'
+        }
+
+        var map = new google.maps.Map(document.getElementById('{{ id }}'), mapOptions);
+        var infoWindow = new google.maps.InfoWindow(), marker, i;
+        // Add intersection observer to add animation class once marker is visible.
+        var intersectionObserver = new IntersectionObserver((entries) => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('{{ markeranimation }}');
+                    intersectionObserver.unobserve(entry.target);
+                }
+            }
+        });
+
+        // place each marker on the map  
+        for( i = 0; i < markers{{ id }}.length; i++ ) {
+            var position = new google.maps.LatLng(markers{{ id }}[i][0], markers{{ id }}[i][1]);
+            bounds.extend(position);
+	        var glyph = null;
+            if (markers{{ id }}[i][4] != ''){
+            	glyph = markers{{ id }}[i][4];
+            }
+            var pin = new google.maps.marker.PinElement({
+                background: '#FE7569',
+                borderColor: '#000',
+                scale: 1,
+                glyph: glyph
+            });
+            marker = new google.maps.marker.AdvancedMarkerElement({
+                position: position,
+                map: map,
+                title: markers{{ id }}[i][2],
+                content: pin.element
+            });
+
+	        const content = marker.content;
+
+    	    {% if markeranimation -%}
+            // Drop animation should be onetime so remove class once animation ends.
+		        {% if markeranimation == 'drop' -%}
+                    content.style.opacity = ""0"";
+		            content.addEventListener('animationend', (event) => {
+                        content.classList.remove('{{ markeranimation }}');
+                        content.style.opacity = ""1"";
+                    });
+                {% endif -%}
+                intersectionObserver.observe(content);
+            {% endif -%}
+
+            // Add info window to marker
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    if (markers{{ id }}[i][3] != ''){
+                        infoWindow.setContent(markers{{ id }}[i][3]);
+                        infoWindow.open(map, marker);
+                    }
+                }
+            })(marker, i));
+        }
+
+        // Center the map to fit all markers on the screen
+        {% if zoom == """" and center == """" and markerCount > 1 -%}
+            map.fitBounds(bounds);
+        {% endif -%}
+
+        // Resize Function
+        google.maps.event.addDomListener(window, ""resize"", function() {
+            var center = map.getCenter();
+            if ( center ) {
+                google.maps.event.trigger(map, ""resize"");
+                map.setCenter(center);
+            }
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize{{ id }});
+
+</script>
+";
+
+            var sql = @"
+-- Update Shortcode: Google Maps
+UPDATE [dbo].[LavaShortcode] SET [Markup]=N'$markup'
+WHERE ([Guid]='FE298210-1307-49DF-B28B-3735A414CCA0')
+";
+            markup = markup.Replace( "'", "''" ).Trim();
+            sql = sql.Replace( "$markup", markup );
+            Sql( sql );
+        }
+        
+        /// <summary>
+        /// Operations to be performed during the downgrade process.
+        /// </summary>
+        public override void Down()
+        {
+            var markup = @"
+{% capture singleQuote %}'{% endcapture %}
+{% capture escapedQuote %}\'{% endcapture %}
+{% assign apiKey = 'Global' | Attribute:'GoogleApiKey' %}
+{% assign url = 'key=' | Append:apiKey %}
+{% assign id = uniqueid | Replace:'-','' %}
+
+{% if apiKey == """" %}
+    <div class=""alert alert-warning"">
+        There is no Google API key defined. Please add your key under: 'Admin Tools > General Settings > Global Attributes > Google API Key'.
+    </div>
+{% endif %}
+
+{% assign markerCount = markers | Size -%}
+
+{% assign mapCenter = center | Trim %}
+{% if mapCenter == """" and markerCount > 0 -%}
+    {% assign centerPoint = markers | First %}
+    {% assign mapCenter = centerPoint.location %}
+{% endif %}
+
+{% assign mapZoom = zoom | Trim %}
+{% if mapZoom == """" %}
+    {% if markerCount == 1 -%}
+        {% assign mapZoom = '11' %}
+    {% else %}
+        {% assign mapZoom = '10' %}
+    {% endif %}
+{% endif %}
+
+{% javascript id:'googlemapsapi' url:'{{ ""https://maps.googleapis.com/maps/api/js?key="" | Append:apiKey }}' %}{% endjavascript %}
+
+{% case markeranimation %}
+{% when 'drop' %}
+    {% assign markeranimation = 'google.maps.Animation.DROP' %}
+{% when 'bounce' %}
+    {% assign markeranimation = 'google.maps.Animation.BOUNCE' %}
+{% else %}
+    {% assign markeranimation = 'null' %}
+{% endcase %}
+
+{% stylesheet %}
+
+.{{ id }} {
+    width: {{ width }};
+}
+
+#map-container-{{ id }} {
+    position: relative;
+}
+
+#{{ id }} {
+    height: {{ height }};
+    overflow: hidden;
+    padding-bottom: 22.25%;
+    padding-top: 30px;
+    position: relative;
+}
+
+{% endstylesheet %}
+
+<div class=""map-container {{ id }}"">
+    <div id=""map-container-{{ id }}""></div>
+    <div id=""{{ id }}""></div>
+</div>	
+
+<script>
+    // create javascript array of marker info
+    var markers{{ id }} = [
+        {% for marker in markers -%}
+            {% assign title = '' -%}
+            {% assign content = '' -%}
+            {% assign icon = '' -%}
+            {% assign location = marker.location | Split:',' -%}
+            {% if marker.title and marker.title != '' -%}
+                {% assign title = marker.title | HtmlDecode | Replace:singleQuote,escapedQuote -%}
+            {% endif -%}
+            {% if marker.content != '' -%}
+                {% assign content = marker.content | StripNewlines | HtmlDecode | Replace:singleQuote,escapedQuote -%}
+            {% endif -%}
+            {% if marker.icon and marker.icon != '' -%}
+                {% assign icon = marker.icon -%}
+            {% endif -%}
+            [{{ location[0] }}, {{ location[1] }},'{{ title }}','{{ content }}','{{ icon }}'],
+        {% endfor -%}
+    ];
+
+    //Set Map
+    function initialize{{ id }}() {
+        var bounds = new google.maps.LatLngBounds();
+        var centerLatLng = new google.maps.LatLng( {{ mapCenter }} );
+        if ( isNaN( centerLatLng.lat() ) || isNaN( centerLatLng.lng() ) ) {
+            centerLatLng = null;
+        };
+
+        var mapOptions = {
+            zoom: {{ mapZoom }},
+            scrollwheel: {{ scrollwheel }},
+            draggable: {{ draggable }},
+            center: centerLatLng,
+            mapTypeId: '{{ maptype }}',
+            zoomControl: {{ showzoom }},
+            mapTypeControl: {{ showmaptype }},
+            gestureHandling: '{{ gesturehandling }}',
+            streetViewControl: {{ showstreetview }},
+            fullscreenControl: {{ showfullscreen }}
+            {% if style and style.content != """" %}
+                ,styles: {{ style.content | StripNewlines | Trim }}
+            {% endif %}
+        }
+
+        var map = new google.maps.Map(document.getElementById('{{ id }}'), mapOptions);
+        var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+        // place each marker on the map  
+        for( i = 0; i < markers{{ id }}.length; i++ ) {
+            var position = new google.maps.LatLng(markers{{ id }}[i][0], markers{{ id }}[i][1]);
+            bounds.extend(position);
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                animation: {{ markeranimation }},
+                title: markers{{ id }}[i][2],
+                icon: markers{{ id }}[i][4]
+            });
+
+            // Add info window to marker
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    if (markers{{ id }}[i][3] != ''){
+                        infoWindow.setContent(markers{{ id }}[i][3]);
+                        infoWindow.open(map, marker);
+                    }
+                }
+            })(marker, i));
+        }
+
+        // Center the map to fit all markers on the screen
+        {% if zoom == """" and center == """" and markerCount > 1 -%}
+            map.fitBounds(bounds);
+        {% endif -%}
+
+        // Resize Function
+        google.maps.event.addDomListener(window, ""resize"", function() {
+            var center = map.getCenter();
+            if ( center ) {
+                google.maps.event.trigger(map, ""resize"");
+                map.setCenter(center);
+            }
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize{{ id }});
+
+</script>
+";
+
+            var sql = @"
+-- Update Shortcode: Google Maps
+UPDATE [dbo].[LavaShortcode] SET [Markup]=N'$markup'
+WHERE ([Guid]='FE298210-1307-49DF-B28B-3735A414CCA0')
+";
+            markup = markup.Replace( "'", "''" ).Trim();
+            sql = sql.Replace( "$markup", markup );
+            Sql( sql );
+        }
+    }
+}
